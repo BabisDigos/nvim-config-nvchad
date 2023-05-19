83432800912a4f055a1c847a37c1eb1383840f4b
Babis Digos <85764745+BabisDigos@users.noreply.github.com>
2023-05-19 16:17:18 +0300

    First commit

---
+++ custom/init.lua
@@ -0,0 +1,16 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+-- My auto commands
+
+-- Cursor
+autocmd("BufEnter", { command = "set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"})
+
+-- Copilot
+autocmd("BufEnter", { command = "Copilot suggestion"})
+

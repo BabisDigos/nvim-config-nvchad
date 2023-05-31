01bcb3d9bd7e05e0831ed1a9c62a1d303a02d684
Harry Digos <85764745+BabisDigos@users.noreply.github.com>
2023-05-31 09:59:53 +0300

    Telescope resume + relative number

--- custom/init.lua
+++ custom/init.lua
@@ -17,9 +17,11 @@ autocmd("BufEnter", { command = "Copilot suggestion" })
 -- Visual mode color
 autocmd("VimEnter", { command = "hi Visual guifg=#000000 guibg=#F4F4F5" })
 
+-- Toggle relative number
+autocmd("VimEnter", { command = ":set rnu!" })
+
 -- Format on save
 -- On save also works eslint fix. For that you need to install eslint globally
 -- npm i -g vscode-langservers-extracted
 -- ./configs/lspconfig.lua has eslint fix on save too
 autocmd("BufWritePre", { command = "lua vim.lsp.buf.format()" })
-
--- custom/mappings.lua
+++ custom/mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>fr"] = { ":Telescope resume <CR>", opts = { nowait = true } },
   },
 }
 
